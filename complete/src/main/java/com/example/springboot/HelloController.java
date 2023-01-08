cddd615294bb0889347724de8059b5f57e5beda7
Dan Giddins <daniel.giddins@nottingham.ac.uk>
2023-01-08 18:14:53 +0000

    add stream logic

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -1,5 +1,8 @@
 package com.example.springboot;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,22 @@ public class HelloController {
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
+	}
+
+	@GetMapping("/testStream")
+	public String testStream() {
+		String[] array = { "asd", "sdfsdf", "fdsgjkdfsbgjklfdgnkldf", "sdfjksdf", "dfsd", "sdfsdjkfsdjklfdsf" };
+		var list = Arrays.asList(array);
+		var stream = list.stream();
+		var filter = stream.filter(x -> x.length() > 5);
+		// var map = filter.map(x -> x.toUpperCase());
+		var map = filter.map(String::toUpperCase);
+		var reduce = map.reduce(
+				"BANANA",
+				(curr, next) -> curr.length() > next.length()
+						? curr
+						: next);
+		var collect = map.collect(Collectors.toList());
+		return reduce;
 	}
 }
