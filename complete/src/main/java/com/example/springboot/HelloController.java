60d136deac316a1344980f3584f1313e68678b2e
Dan Giddins <daniel.giddins@nottingham.ac.uk>
2023-01-08 18:27:22 +0000

    now works

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -14,20 +14,20 @@ public class HelloController {
 		return "Greetings from Spring Boot!";
 	}
 
-	@GetMapping("/testStream")
+	@GetMapping("/teststream")
 	public String testStream() {
-		String[] array = { "asd", "sdfsdf", "fdsgjkdfsbgjklfdgnkldf", "sdfjksdf", "dfsd", "sdfsdjkfsdjklfdsf" };
+		String[] array = { "asd", "sdfsdf", "fdsgjkdfsbgjklfdgnkldf", "sdfjksdf", "dfsd", "123456789",
+				"sdfsdjksdfdsfdsfsdfdsfdsfdfsdjklfdsf" };
 		var list = Arrays.asList(array);
 		var stream = list.stream();
-		var filter = stream.filter(x -> x.length() > 5);
-		// var map = filter.map(x -> x.toUpperCase());
-		var map = filter.map(String::toUpperCase);
-		var reduce = map.reduce(
+		var filter = stream.filter(x -> x.length() < 10);
+		var map = filter.map(x -> x.toUpperCase() + "upper");
+		var result = map.reduce(
 				"BANANA",
 				(curr, next) -> curr.length() > next.length()
 						? curr
 						: next);
-		var collect = map.collect(Collectors.toList());
-		return reduce;
+		// var result = map.collect(Collectors.toList());
+		return result.toString();
 	}
 }
