91081c3dd93751d943fb53511df8c6e03292c84a
Dan Giddins <daniel.giddins@nottingham.ac.uk>
2023-01-10 10:08:09 +0000

    formatting

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -2,7 +2,6 @@ package com.example.springboot;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,17 +15,25 @@ public class HelloController {
 
 	@GetMapping("/teststreams")
 	public String testStreams() {
-		String[] myArray = { "asd", "sdfsdf", "fdsgjkdfsbgjklfdgnkldf", "sdfjksdf", "dfsd", "12345678",
-				"sdfsdjksdfdsfdsfsdfdsfdsfdfsdjklfdsf" };
+		String[] myArray = {
+				"asd",
+				"sdfsdf",
+				"fdsgjkdfsbgjklfdgnkldf",
+				"sdfjksdf",
+				"dfsd",
+				"12345678",
+				"sdfsdjksdfdsfdsfsdfdsfdsfdfsdjklfdsf",
+		};
 		var myList = Arrays.asList(myArray);
-		var longest = myList.stream()
+		var longest = myList
+				.stream()
 				.filter(x -> x.length() < 10)
 				.map(x -> x.toUpperCase() + "upper")
-				.reduce("BANANA",
-						(curr, next) -> curr.length() > next.length()
-								? curr
-								: next);
-		var tooBig = myList.stream()
+				.reduce(
+						"BANANA",
+						(curr, next) -> curr.length() > next.length() ? curr : next);
+		var tooBig = myList
+				.stream()
 				.filter(x -> x.length() >= 10)
 				.collect(Collectors.toList());
 		return longest + " but also " + tooBig.toString();
